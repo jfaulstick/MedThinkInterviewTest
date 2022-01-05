22131ae256991dd5f5164abfdd506f6bfaae1b9d
Joe Faulstick <jfaulstick@gmail.com>
2022-01-04 23:12:05 -0500

    Initial first pass at home page

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,13 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('pages/index', { 
-    title: 'Express' 
+    title: 'Joe Faulstick' 
+  });
+});
+
+router.get('/quiz', function(req, res, next) {
+  res.render('pages/quiz', {
+    title: 'Quiz'
   });
 });
 
