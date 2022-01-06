ec301cb169c3c7970345238b84322e1190496f09
Joe Faulstick <jfaulstick@gmail.com>
2022-01-06 09:55:09 -0500

    Finalized questions for quiz

--- routes/index.js
+++ routes/index.js
@@ -17,9 +17,9 @@ router.get('/quiz', function(req, res, next) {
   res.render('pages/quiz', {
     title: 'Quiz',
     questions: [
-      {id: '1', string: 'Question 1'},
-      {id: '2', string: 'Question 2'},
-      {id: '3', string: 'Question 3'},
+      {id: '1', string: 'Joe has worked on at least one "Game of the Year" winning title.'},
+      {id: '2', string: 'Joe has visited 7 countries other than the US.'},
+      {id: '3', string: 'Joe is born and raised in North Carolina.'},
     ],
     answer: answerString || 'Select your answer!'
   });
